c5e6ebe5d001ba2ca015341f3b710171f0782d62
Артём Шевляков <gr636_sheaan@lab-305-5.tomtit-tomsk.ru>
2025-04-24 12:51:20 +0700

    TryExcept

--- Lab_4/Task2.py
+++ Lab_4/Task2.py
@@ -2,7 +2,10 @@ import random
 list = []
 a = 0
 for i in range(14):
-    i = int(input("Enter a number: "))
+    try:
+        i = int(input("Enter a number: "))
+    except ValueError:
+        print("Please enter a number")
     a = a + i
     list.append(i)
 list.append(a)
